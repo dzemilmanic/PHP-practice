772421ccb8d5573dbbc7aa77adf826c87e2fbacb
Džemil Manić <dzemilmanic@hotmail.com>
2024-10-29 08:41:57 +0100

    primer 8

---
+++ predavanja4/primer8/login.php
@@ -0,0 +1,65 @@
+<?php include "navigacija.php" ?>
+<main>
+    <?php
+    // stanje da li su podaci prosleđeni
+    $poslato = false;
+
+    if (isset($_GET['korisnickoIme']) && isset($_GET['lozinka'])) {
+        $poslato = true;
+        $korisnik = $_GET['korisnickoIme'];
+    }
+    ?>
+    <div>
+        <h1>Prijava</h1><br>
+        <!-- method="GET" govori browser-u da vrednosti koje korisnik upiše u formular doda URL adresi
+     -- pritiskom na submit dugme poziva se program zadat kao action sa prosleđenim argumentima 
+     -- tako na primer ako u polje korisničko ime upišete Paja
+     -- a u polje lozinka Pata nakon submita dobijate	
+     -- login.php?korisnickoIme=Paja&lozinka=Pata-->
+
+
+        <form action="login.php" method="GET" onsubmit="return provera()">
+            <div>
+                <label>Korisničko ime</label>
+                <input type="text" class="form-control" name="korisnickoIme" id="username" autofocus>
+            </div>
+            <div>
+                <label>Lozinka</label>
+                <input type="password" class="form-control" name="lozinka" id="password">
+            </div>
+            <?php
+            if ($poslato)
+                echo "<p class='text-danger'>Trenutno je poslat zahtev za logovanje korisnika <b>$korisnik</b> </p>";
+            else
+
+                echo "<p class='text-danger'>Trenutno nema poslatih zahteva za logovanje </p>";
+            ?>
+            <a href="$">Zaboravio sam lozinku</a><br><br>
+            <button type="submit">Prijavi se</button>
+        </form>
+        <!-- Za validaciju polja koristi se JS 
+      -- funkcija proverava da li su polja username i password popunjena
+      -- i daje obaveštenje o nepopunjenom polju
+      -->
+        <script>
+            function provera() {
+                var username = document.getElementById('username').value.trim();
+                var password = document.getElementById('password').value.trim();
+
+                if (username.length == 0) {
+                    alert("Nije uneto korisničko ime!");
+                    return false;
+                }
+
+                if (password.length == 0) {
+                    alert("Nije uneta lozinka!");
+                    return false;
+                }
+
+                return true;
+            }
+        </script>
+        <br>Nemate nalog? <a href="registracija.php">Registrujte se</a><br>
+    </div>
+</main>
+<?php include "footer.php" ?>
---
+++ predavanja4/primer8/navigacija.php
@@ -0,0 +1,59 @@
+<!-- navigacija.php -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="autor" content="Ulfeta Marovac">
+    <meta name="description" content="Superglobana promenljiva $_SERVER">
+    <meta name="keywords" content="PHP, SERVER">
+    <title>Primer 7: $_SERVER</title>
+<style>
+body {
+  margin: 0px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+main {
+  margin: 30px;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+<body>
+<?php
+// Preuzimamo informacije o fajlu iz koga je pokrenuta navigacija
+$putanja = $_SERVER["SCRIPT_NAME"];
+// Poslednja pozicija znaka '/'
+$pozicija=strrpos($_SERVER["SCRIPT_NAME"], "/");
+$naziv_fajla=substr($_SERVER["SCRIPT_NAME"], $pozicija + 1);
+?>
+<div class="topnav">
+<!-- postavlja element navigacije stranice koja je trenutno ucitana kao aktivan-->
+  <a class=" <?php if ($naziv_fajla === "index.php") echo 'active'; ?>"  href="index.php"> Početna</a>
+   <a class=" <?php if ($naziv_fajla === "anketa.php") echo 'active'; ?>" href="anketa.php"> Anketa</a>
+  <a class=" <?php if ($naziv_fajla === "rezultati.php") echo 'active'; ?>" href="rezultati.php">Rezultati ankete</a>
+   <a class=" <?php if ($naziv_fajla === "login.php") echo 'active'; ?>" href="login.php"> Prijavi se  </a>
+</div>
+
+
