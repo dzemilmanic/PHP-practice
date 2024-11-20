9c5010e1047d9c638000667e75fd712adef758a2
Džemil Manić <dzemilmanic@hotmail.com>
2024-11-20 22:27:35 +0100

    Create vezbe1.php

---
+++ vezbe2/vezbe1.php
@@ -0,0 +1,21 @@
+<?php
+    $naslov = "Vezbe iz php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php
+        echo "$naslov";
+        ?>
+    </title>
+</head>
+<body>
+    <h1><?php
+            echo "$naslov";
+        ?>
+    </h1>
+</body>
+</html>
