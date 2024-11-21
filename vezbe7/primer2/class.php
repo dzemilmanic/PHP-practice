a2c344e85377c7dad8c11725009518b435617c96
Džemil Manić <dzemilmanic@hotmail.com>
2024-11-21 20:06:13 +0100

    Create class.php

---
+++ vezbe7/primer2/class.php
@@ -0,0 +1,51 @@
+
+<?php
+class Knjiga {
+    private $naslov;
+    private $ISBN;
+    private $autor;
+    function __construct($naslov, $ISBN, $autor) {
+        $this->naslov = $naslov;
+        $this->ISBN = $ISBN;
+        $this->autor = $autor;
+    }
+    function getNaslov() {
+        return $this->naslov;
+    }
+    function getISBN() {
+        return $this->ISBN;
+    }
+    function getAutor() {
+        return $this->autor;
+    }
+    
+
+}
+$knjiga1 = new Knjiga("knjiga1", "1234", "autor1");
+$knjiga2 = new Knjiga("knjiga2", "12345", "autor2");
+$knjiga3 = new Knjiga("knjiga3", "12346", "autor3");
+$niz = [
+    $knjiga1,
+    $knjiga2, 
+    $knjiga3
+];
+
+
+
+?>
+<table border=1>
+    <tr>
+        <th>Naslov</th>
+        <th>Autor</th>
+        <th>isbn</th>
+    <tr>
+    <?php
+    foreach($niz as $knjiga) {
+        echo "<tr>
+                <td>{$knjiga->getNaslov()}</td>
+                <td>{$knjiga->getAutor()}</td>
+                <td>{$knjiga->getISBN()}</td>
+            <tr>";
+    }
+    ?>
+</table>
